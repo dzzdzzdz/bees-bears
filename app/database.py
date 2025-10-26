dd7c6ebcab035ff576a4446aafc75cb24abc72de
Daniel Zhao <danielzhaozd@gmail.com>
2025-10-26 16:05:00 +0100

    cleanup

--- app/database.py
+++ app/database.py
@@ -3,12 +3,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv(dotenv_path=".env")
 
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "postgresql://postgres:postgres@localhost:5432/bees_and_bears"
+    "postgresql+psycopg://postgres:postgres@localhost:5432/bees_and_bears"
 )
+
+print("DATABASE_URL", DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
